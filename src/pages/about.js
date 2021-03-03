91422a20af11dcd502bb160b14440054b3409636
lymarrie <16665882+lymarrie@users.noreply.github.com>
2021-03-02 23:46:52 -0500

    updated the "about" page... #gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ export default function About({ data }) {
       <h1>About {data.site.siteMetadata.title}</h1>
       <p>
         We're the only site running on your computer dedicated to showing the
-        best photos and videos of pandas eating lots of food.
+        best photos and videos of pandas eating lots of food. Also Luc is a Gatsby pro.
       </p>
     </Layout>
   )
